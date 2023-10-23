889ea66143be40ba6930e233cdbf34fb5f078f33
ApMatheus <matheusaparecido101@gmail.com>
2023-10-23 09:00:25 -0300

    fix(Hero) ajusts in types

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -11,15 +11,23 @@ export interface Paragraph {
   bold?: string;
 }
 
+export interface Button {
+  label: string;
+  href: string;
+}
+
 export interface Props {
   title?: TitleHero;
   /** @format textarea */
   paragraph?: Paragraph;
-  btnLabel: string;
+  button: Button;
   image: DecoImage;
+  altImage: string;
 }
 
-export default function Hero({ title, paragraph, image, btnLabel }: Props) {
+export default function Hero(
+  { title, paragraph, image, button, altImage }: Props,
+) {
   return (
     <section
       className={"px-6 bg-primary h-screen w-full grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 justify-center items-center md:gap-6 xl:px-16"}
@@ -47,17 +55,18 @@ export default function Hero({ title, paragraph, image, btnLabel }: Props) {
           className={"my-8 fixed bottom-6 flex justify-center md:justify-start md:static md:left-0 md:translate-x-0 w-full left-[50%] translate-x-[-50%]"}
         >
           <a
-            className={"bg-secondary rounded-[32px] px-7 py-3 flex justify-center items-center w-auto w-[256px] "}
+            className={"bg-secondary rounded-[32px] px-7 py-3 flex justify-center items-center w-[256px] "}
+            href={button.href}
           >
             <p className={" font-bold text-base-100"}>
-              {btnLabel}
+              {button.label}
             </p>
           </a>
         </div>
       </div>
       <div className={""}>
         <picture className={"md:px-3"}>
-          <image src={image} className={"md:px-8"} />
+          <image src={image} className={"md:px-8"} alt={altImage} />
         </picture>
       </div>
     </section>
