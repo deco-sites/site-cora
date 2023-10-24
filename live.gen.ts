1b6d44fb4ff509f820df935db022c2c3d5cef336
ApMatheus <matheusaparecido101@gmail.com>
2023-10-24 09:22:28 -0300

    feat(Articles) created

--- live.gen.ts
+++ live.gen.ts
@@ -6,15 +6,16 @@ import * as $$$$0 from "./routes/styles.css.ts";
 import * as $$$$1 from "./routes/_app.tsx";
 import * as $$$$$0 from "./islands/MenuTable.tsx";
 import * as $$$$$1 from "./islands/SliderJS.tsx";
-import * as $$$$$$$$0 from "./sections/Benefit.tsx";
-import * as $$$$$$$$1 from "./sections/CardsSection.tsx";
-import * as $$$$$$$$2 from "./sections/Footer.tsx";
-import * as $$$$$$$$3 from "./sections/Hero.tsx";
-import * as $$$$$$$$4 from "./sections/ParagraphList.tsx";
-import * as $$$$$$$$5 from "./sections/SimpleTextList.tsx";
-import * as $$$$$$$$6 from "./sections/SlideNews.tsx";
-import * as $$$$$$$$7 from "./sections/TableInfos.tsx";
-import * as $$$$$$$$8 from "./sections/Theme.tsx";
+import * as $$$$$$$$0 from "./sections/Articles.tsx";
+import * as $$$$$$$$1 from "./sections/Benefit.tsx";
+import * as $$$$$$$$2 from "./sections/CardsSection.tsx";
+import * as $$$$$$$$3 from "./sections/Footer.tsx";
+import * as $$$$$$$$4 from "./sections/Hero.tsx";
+import * as $$$$$$$$5 from "./sections/ParagraphList.tsx";
+import * as $$$$$$$$6 from "./sections/SimpleTextList.tsx";
+import * as $$$$$$$$7 from "./sections/SlideNews.tsx";
+import * as $$$$$$$$8 from "./sections/TableInfos.tsx";
+import * as $$$$$$$$9 from "./sections/Theme.tsx";
 import * as $live_workflows from "$live/routes/live/workflows/run.ts";
 import * as $live_middleware from "$live/routes/_middleware.ts";
 import * as $live_workbench from "$live/routes/live/workbench.ts";
@@ -172,15 +173,16 @@ const manifest = {
     "$live/sections/PageInclude.tsx": i2$$$$$2,
     "$live/sections/Slot.tsx": i2$$$$$3,
     "$live/sections/UseSlot.tsx": i2$$$$$4,
-    "deco-sites/site-cora/sections/Benefit.tsx": $$$$$$$$0,
-    "deco-sites/site-cora/sections/CardsSection.tsx": $$$$$$$$1,
-    "deco-sites/site-cora/sections/Footer.tsx": $$$$$$$$2,
-    "deco-sites/site-cora/sections/Hero.tsx": $$$$$$$$3,
-    "deco-sites/site-cora/sections/ParagraphList.tsx": $$$$$$$$4,
-    "deco-sites/site-cora/sections/SimpleTextList.tsx": $$$$$$$$5,
-    "deco-sites/site-cora/sections/SlideNews.tsx": $$$$$$$$6,
-    "deco-sites/site-cora/sections/TableInfos.tsx": $$$$$$$$7,
-    "deco-sites/site-cora/sections/Theme.tsx": $$$$$$$$8,
+    "deco-sites/site-cora/sections/Articles.tsx": $$$$$$$$0,
+    "deco-sites/site-cora/sections/Benefit.tsx": $$$$$$$$1,
+    "deco-sites/site-cora/sections/CardsSection.tsx": $$$$$$$$2,
+    "deco-sites/site-cora/sections/Footer.tsx": $$$$$$$$3,
+    "deco-sites/site-cora/sections/Hero.tsx": $$$$$$$$4,
+    "deco-sites/site-cora/sections/ParagraphList.tsx": $$$$$$$$5,
+    "deco-sites/site-cora/sections/SimpleTextList.tsx": $$$$$$$$6,
+    "deco-sites/site-cora/sections/SlideNews.tsx": $$$$$$$$7,
+    "deco-sites/site-cora/sections/TableInfos.tsx": $$$$$$$$8,
+    "deco-sites/site-cora/sections/Theme.tsx": $$$$$$$$9,
     "deco-sites/std/sections/Analytics.tsx": i2$$$$$5,
     "deco-sites/std/sections/configButterCMS.global.tsx": i2$$$$$6,
     "deco-sites/std/sections/configNuvemShop.tsx": i2$$$$$7,
